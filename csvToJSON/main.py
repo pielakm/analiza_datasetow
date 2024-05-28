95193774e7ac9ad72d02bae9b739fedaaebc1836
PewkaP <panplewka@gmail.com>
2024-05-28 10:26:10 +0200

    ceny surowców do pliku json

---
+++ csvToJSON/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Load the CSV file
+csv_file_path = '../commodity_prices_filtered.csv'  # The path to the uploaded CSV file
+df = pd.read_csv(csv_file_path)
+
+# Convert the DataFrame to JSON format
+json_data = df.to_json(orient='records', date_format='iso')
+
+# Save the JSON data to a file
+json_file_path = '../commodity_prices_filtered.json'
+with open(json_file_path, 'w') as json_file:
+    json_file.write(json_data)
+
+print(f"Data has been successfully converted to JSON and saved to {json_file_path}")
